9f3d5f63de18f28b0e4de5c0e15c401953589c5f
Daniel Koop <mail@danielkoop.me>
2016-03-17 18:14:30 +0100

    Added field type setters.

--- spec/generatorSpec.php
+++ spec/generatorSpec.php
@@ -35,6 +35,12 @@ class generatorSpec extends ObjectBehavior
 	}
 
 
+	/******************************************************************
+	*                                                                 *
+	*                       TESTING FIELD TYPES                       *
+	*                                                                 *
+	******************************************************************/
+
 	public function it_can_set_the_field_type_checkbox()
 	{
 		// Preload the required fields
@@ -47,14 +53,105 @@ class generatorSpec extends ObjectBehavior
 								'type'			=> 'checkbox'
 							  ];
 
+		// Set the checkbox via the general setter
+		$this->set_as('checkbox')
+			 ->shouldReturn($this);
+
 		// Set the checkbox
 		$this->set_as_checkbox()
 			 ->shouldReturn($this);
 
 		$this->get_mapped_fields()
 			 ->shouldBe($expected_mapped_fields);
-
 	}
+
+	public function it_can_set_the_field_type_radiobutton()
+	{
+		// Preload the required fields
+		$this->preload_one_field();
+
+		// Set the expected settings
+		$expected_mapped_fields[]    	= [
+								'name' 			=> $this->default['field_name'],
+								'translation' 	=> $this->default['translation'],
+								'type'			=> 'radiobutton'
+							  ];
+
+		// Set the radiobutton via the general setter
+		$this->set_as('radiobutton')
+			 ->shouldReturn($this);
+
+		// Set the radiobutton
+		$this->set_as_radiobutton()
+			 ->shouldReturn($this);
+
+		$this->get_mapped_fields()
+			 ->shouldBe($expected_mapped_fields);
+	}
+
+	public function it_can_set_the_field_type_dropdown()
+	{
+		// Preload the required fields
+		$this->preload_one_field();
+
+		// Set the expected settings
+		$expected_mapped_fields[]    	= [
+								'name' 			=> $this->default['field_name'],
+								'translation' 	=> $this->default['translation'],
+								'type'			=> 'dropdown'
+							  ];
+		
+		// Set the dropdown via the general setter
+		$this->set_as('dropdown')
+			 ->shouldReturn($this);
+
+		// Set the dropdown
+		$this->set_as_dropdown()
+			 ->shouldReturn($this);
+
+		$this->get_mapped_fields()
+			 ->shouldBe($expected_mapped_fields);
+	}
+
+	public function it_can_set_the_field_type_text()
+	{
+		// Preload the required fields
+		$this->preload_one_field();
+
+		// Set the expected settings
+		$expected_mapped_fields[]    	= [
+								'name' 			=> $this->default['field_name'],
+								'translation' 	=> $this->default['translation'],
+								'type'			=> 'text'
+							  ];
+		
+		// Set the text via the general setter
+		$this->set_as('text')
+			 ->shouldReturn($this);
+
+		// Set the dropdown
+		$this->set_as_text()
+			 ->shouldReturn($this);
+
+		$this->get_mapped_fields()
+			 ->shouldBe($expected_mapped_fields);
+	}
+
+	/******************************************************************
+	*                                                                 *
+	*                     TESTING LOADING METHODS                     *
+	*                                                                 *
+	******************************************************************/
+
+	// public function it_can_load_data_from_a_taxonomy()
+	// {
+	// 	// @todo
+	// }
+
+	// public function it_can_load_data_from_an_array()
+	// {
+	// 	// @todo
+	// }
 
 
 	/** Used for preloading. We can not do this for every test (it_can_add_a_field does not need this) */
--- src/generator.php
+++ src/generator.php
@@ -25,13 +25,61 @@ class generator
         return $this->mapped_fields;
     }
 
-    public function set_as_checkbox()
+    /**
+     * Set the field type to $type
+     * 
+     * @param  string $type The type (like checkbox, dropdown, etc)
+     * @return  object Returns $this
+     */
+    public function set_as($type)
     {
         end($this->mapped_fields);
         $key = key($this->mapped_fields);
 
-        $this->mapped_fields[ $key ]['type'] = 'checkbox';
+        $this->mapped_fields[ $key ]['type'] = $type;
 
         return $this;
     }
+
+    /**
+     * Sets the last field type to checkbox
+     *
+     * @return  object Returns $this
+     */
+    public function set_as_checkbox()
+    {
+        return $this->set_as('checkbox');
+    }
+
+    /**
+     * Sets the last field type to radiobutton
+     *
+     * @return  object Returns $this
+     */
+    public function set_as_radiobutton()
+    {
+        return $this->set_as('radiobutton');
+    }
+
+    /**
+     * Sets the last field type to dropdown
+     *
+     * @return  object Returns $this
+     */
+    public function set_as_dropdown()
+    {
+        return $this->set_as('dropdown');
+    }
+
+    /**
+     * Sets the last field type to text
+     *
+     * @return  object Returns $this
+     */
+    public function set_as_text()
+    {
+        return $this->set_as('text');
+    }
+
+    
 }
