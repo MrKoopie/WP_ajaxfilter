510ab5099f704437581aa2d0da145bc79c2f8197
D Koop <mail@danielkoop.me>
2016-05-18 02:31:06 -0400

    Applied fixes from StyleCI

--- spec/stubSpec.php
+++ spec/stubSpec.php
@@ -2,154 +2,153 @@
 
 namespace spec\MrKoopie\WP_ajaxfilter;
 
-use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 use Mockery;
-
+use PhpSpec\ObjectBehavior;
 
 class stubSpec extends ObjectBehavior
 {
-	// Set up the mocks
-	protected $mockery_original_fileystem;
-	protected $mockery_overridden_fileystem;
-	protected $mockery_local;
-	protected $mockery_WP_wrapper;
+    // Set up the mocks
+    protected $mockery_original_fileystem;
+    protected $mockery_overridden_fileystem;
+    protected $mockery_local;
+    protected $mockery_WP_wrapper;
 
-	// Set up the default data
-	protected $default = [
-						'filename'              => 'some_filename',
-						'non_existing_filename' => 'non_existing_filename',
-						];
-    function it_is_initializable()
+    // Set up the default data
+    protected $default = [
+                        'filename'              => 'some_filename',
+                        'non_existing_filename' => 'non_existing_filename',
+                        ];
+
+    public function it_is_initializable()
     {
-    	// @todo Should add a verification if the correct arguments for __construct are provided.
+        // @todo Should add a verification if the correct arguments for __construct are provided.
 
         $this->shouldHaveType('MrKoopie\WP_ajaxfilter\stub');
     }
 
-    function it_can_list_original_stubs()
+    public function it_can_list_original_stubs()
     {
-		$return_list_contents[] = [
-			'filename' => $this->default['filename'],
-			'extension' => 'stub'
-		];
+        $return_list_contents[] = [
+            'filename'  => $this->default['filename'],
+            'extension' => 'stub',
+        ];
 
-		$this->mockery_WP_wrapper
-			->shouldReceive('get_stylesheet_directory')
-			->once();
+        $this->mockery_WP_wrapper
+            ->shouldReceive('get_stylesheet_directory')
+            ->once();
 
-    	$this->mockery_original_fileystem
-    		 ->shouldReceive('listContents')
-			->andReturn($return_list_contents)
-			->once();
+        $this->mockery_original_fileystem
+             ->shouldReceive('listContents')
+            ->andReturn($return_list_contents)
+            ->once();
 
-    	$this->list_original_stubs()
-    		 ->shouldBeArray();
+        $this->list_original_stubs()
+             ->shouldBeArray();
     }
 
-    function it_can_list_overriden_stubs()
+    public function it_can_list_overriden_stubs()
     {
-    	$return_list_contents[] = [
-			'filename' => $this->default['filename'],
-			'extension' => 'stub'
-		];
+        $return_list_contents[] = [
+            'filename'  => $this->default['filename'],
+            'extension' => 'stub',
+        ];
 
-		$this->mockery_WP_wrapper
-			->shouldReceive('get_stylesheet_directory')
-			->once();
+        $this->mockery_WP_wrapper
+            ->shouldReceive('get_stylesheet_directory')
+            ->once();
 
-    	$this->mockery_overridden_fileystem
-    		 ->shouldReceive('listContents')
-			->andReturn($return_list_contents)
-			->once();
+        $this->mockery_overridden_fileystem
+             ->shouldReceive('listContents')
+            ->andReturn($return_list_contents)
+            ->once();
 
-    	$this->list_overriden_stubs()
-    		->shouldBeArray();
+        $this->list_overriden_stubs()
+            ->shouldBeArray();
     }
 
-    function it_can_parse_a_stub()
+    public function it_can_parse_a_stub()
     {
-		$return_list_contents[] = [
-									'filename' => $this->default['filename'],
-									'extension' => 'stub'
-									];
-		$this->mockery_original_fileystem
-			->shouldReceive('listContents')
-			->andReturn($return_list_contents)
-			->once()
-			->shouldReceive('read')
-			->andReturn('<{{replace_this}}>')
-			->once();
+        $return_list_contents[] = [
+                                    'filename'  => $this->default['filename'],
+                                    'extension' => 'stub',
+                                    ];
+        $this->mockery_original_fileystem
+            ->shouldReceive('listContents')
+            ->andReturn($return_list_contents)
+            ->once()
+            ->shouldReceive('read')
+            ->andReturn('<{{replace_this}}>')
+            ->once();
 
-		$this->mockery_overridden_fileystem
-    		 ->shouldReceive('listContents')
-			->andReturn([])
-			->once();
+        $this->mockery_overridden_fileystem
+             ->shouldReceive('listContents')
+            ->andReturn([])
+            ->once();
 
-    	$parameters = [
-						'replace_this' => 'replacement'
-		];
+        $parameters = [
+                        'replace_this' => 'replacement',
+        ];
 
-		$this->mockery_WP_wrapper
-			->shouldReceive('get_stylesheet_directory')
-			->once();
+        $this->mockery_WP_wrapper
+            ->shouldReceive('get_stylesheet_directory')
+            ->once();
 
-    	$this->parse_stub($this->default['filename'], $parameters)
-    		 ->shouldBe('<replacement>');
+        $this->parse_stub($this->default['filename'], $parameters)
+             ->shouldBe('<replacement>');
     }
 
-    function it_can_return_an_exception_when_a_stub_does_not_exist()
+    public function it_can_return_an_exception_when_a_stub_does_not_exist()
     {
-		$return_list_contents[] = [
-									'filename' => $this->default['filename'],
-									'extension' => 'stub'
-									];
-		$this->mockery_original_fileystem
-			->shouldReceive('listContents')
-			->andReturn($return_list_contents)
-			->once();
+        $return_list_contents[] = [
+                                    'filename'  => $this->default['filename'],
+                                    'extension' => 'stub',
+                                    ];
+        $this->mockery_original_fileystem
+            ->shouldReceive('listContents')
+            ->andReturn($return_list_contents)
+            ->once();
 
-    	$parameters = [
-						'replace_this' => 'replacement'
-		];
+        $parameters = [
+                        'replace_this' => 'replacement',
+        ];
 
-		$this->mockery_WP_wrapper
-			->shouldReceive('get_stylesheet_directory')
-			->once();
+        $this->mockery_WP_wrapper
+            ->shouldReceive('get_stylesheet_directory')
+            ->once();
 
-    	$this->shouldThrow('MrKoopie\WP_ajaxfilter\Exceptions\stub_not_found_exception')
-    		->during('parse_stub',[$this->default['non_existing_filename'], $parameters]);
+        $this->shouldThrow('MrKoopie\WP_ajaxfilter\Exceptions\stub_not_found_exception')
+            ->during('parse_stub', [$this->default['non_existing_filename'], $parameters]);
     }
 
-    function it_can_handle_an_overriden_stub()
+    public function it_can_handle_an_overriden_stub()
     {
-    	$return_list_contents[] = [
-									'filename' => $this->default['filename'],
-									'extension' => 'stub'
-									];
-		$this->mockery_original_fileystem
-			->shouldReceive('listContents')
-			->andReturn($return_list_contents)
-			->once();
+        $return_list_contents[] = [
+                                    'filename'  => $this->default['filename'],
+                                    'extension' => 'stub',
+                                    ];
+        $this->mockery_original_fileystem
+            ->shouldReceive('listContents')
+            ->andReturn($return_list_contents)
+            ->once();
 
-		$this->mockery_overridden_fileystem
-    		 ->shouldReceive('listContents')
-			->andReturn($return_list_contents)
-			->once()
-			->shouldReceive('read')
-			->andReturn('<{{replace_this_override}}>')
-			->once();
+        $this->mockery_overridden_fileystem
+             ->shouldReceive('listContents')
+            ->andReturn($return_list_contents)
+            ->once()
+            ->shouldReceive('read')
+            ->andReturn('<{{replace_this_override}}>')
+            ->once();
 
-    	$parameters = [
-						'replace_this_override' => 'replacement'
-		];
+        $parameters = [
+                        'replace_this_override' => 'replacement',
+        ];
 
-		$this->mockery_WP_wrapper
-			->shouldReceive('get_stylesheet_directory')
-			->once();
+        $this->mockery_WP_wrapper
+            ->shouldReceive('get_stylesheet_directory')
+            ->once();
 
-    	$this->parse_stub($this->default['filename'], $parameters)
-    		 ->shouldBe('<replacement>');
+        $this->parse_stub($this->default['filename'], $parameters)
+             ->shouldBe('<replacement>');
     }
 
     /******************************************************************
@@ -157,20 +156,21 @@ class stubSpec extends ObjectBehavior
     *                             VARIOUS                             *
     *                                                                 *
     ******************************************************************/
-	/**
-	 * Initialize the class
-	 */
-	public function let()
-	{
-		$this->mockery_original_fileystem   	= Mockery::mock('League\Flysystem\Filesystem');
-		$this->mockery_overridden_fileystem   	= Mockery::mock('League\Flysystem\Filesystem');
-		$this->mockery_local   		= Mockery::mock('League\Flysystem\Adapter\Local');
-		$this->mockery_WP_wrapper 	= Mockery::mock('MrKoopie\WP_wrapper\WP_wrapper');
-		$this->beConstructedWith($this->mockery_WP_wrapper, $this->mockery_local, $this->mockery_original_fileystem, $this->mockery_overridden_fileystem);
-	}
 
-	public function letGo()
-	{
-		Mockery::close();
-	}
+    /**
+     * Initialize the class.
+     */
+    public function let()
+    {
+        $this->mockery_original_fileystem = Mockery::mock('League\Flysystem\Filesystem');
+        $this->mockery_overridden_fileystem = Mockery::mock('League\Flysystem\Filesystem');
+        $this->mockery_local = Mockery::mock('League\Flysystem\Adapter\Local');
+        $this->mockery_WP_wrapper = Mockery::mock('MrKoopie\WP_wrapper\WP_wrapper');
+        $this->beConstructedWith($this->mockery_WP_wrapper, $this->mockery_local, $this->mockery_original_fileystem, $this->mockery_overridden_fileystem);
+    }
+
+    public function letGo()
+    {
+        Mockery::close();
+    }
 }
--- src/Input/base.php
+++ src/Input/base.php
@@ -1,11 +1,12 @@
 <?php
+
 namespace MrKoopie\WP_ajaxfilter\Input;
-use MrKoopie\WP_wrapper\WP_wrapper;
+
 use MrKoopie\WP_ajaxfilter\stub;
+use MrKoopie\WP_wrapper\WP_wrapper;
 
 /**
- * Class Checkbox
- * @package MrKoopie\WP_ajaxfilter\Input
+ * Class Checkbox.
  */
 class base
 {
@@ -19,7 +20,7 @@ class base
     // protected $filter_options;
 
     /**
-     * Can be taxonomy or array
+     * Can be taxonomy or array.
      */
     protected $data_source;
     protected $filter_data;
@@ -30,35 +31,36 @@ class base
      */
     public function __construct($label, $field_name, $WP_wrapper = null, $stub = null)
     {
-        $this->label     = $label;
+        $this->label = $label;
         $this->field_name = $field_name;
 
-        if($WP_wrapper != null)
+        if ($WP_wrapper != null) {
             $this->WP_wrapper = $WP_wrapper;
-        else
+        } else {
             $this->WP_wrapper = new WP_wrapper();
+        }
 
-        if($stub != null)
+        if ($stub != null) {
             $this->stub = $stub;
-        else
+        } else {
             $this->stub = new stub();
+        }
     }
 
     /**
-     * Load the taxonomy data from WordPress
+     * Load the taxonomy data from WordPress.
      *
      * @param $taxonomy_id The $taxonomy_id of WordPress
      */
     public function load_data_from_taxonomy($taxonomy_id)
     {
-        $this->taxonomy_id   = $taxonomy_id;
-        $this->data_source   = 'taxonomy';
+        $this->taxonomy_id = $taxonomy_id;
+        $this->data_source = 'taxonomy';
 
         $terms = $this->WP_wrapper->get_terms($taxonomy_id);
-        foreach($terms as $taxonomy)
-        {
-            $tmp_taxonomy['slug']    = $taxonomy->slug;
-            $tmp_taxonomy['label']   = $taxonomy->name;
+        foreach ($terms as $taxonomy) {
+            $tmp_taxonomy['slug'] = $taxonomy->slug;
+            $tmp_taxonomy['label'] = $taxonomy->name;
             $tmp_taxonomy['term_id'] = $taxonomy->term_id;
 
             $this->filter_data[] = $tmp_taxonomy;
@@ -67,7 +69,7 @@ class base
     }
 
     /**
-     * Load the array
+     * Load the array.
      *
      * @param array $array The $array in the format []['slug', 'label']
      */
@@ -75,10 +77,9 @@ class base
     {
         $this->data_source = 'array';
 
-        foreach($array as $key => $data)
-        {
-            $tmp_data['slug']    = $data['slug'];
-            $tmp_data['label']   = $data['label'];
+        foreach ($array as $key => $data) {
+            $tmp_data['slug'] = $data['slug'];
+            $tmp_data['label'] = $data['label'];
 
             $this->filter_data[] = $tmp_data;
             unset($tmp_data);
@@ -114,7 +115,7 @@ class base
     }
 
     /**
-     * @return  array The filter data.
+     * @return array The filter data.
      */
     public function get_filter_data()
     {
